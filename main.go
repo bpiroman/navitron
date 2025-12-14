e294f6c352a5077738f1fda7dd384b436acfdd7b
Ben Piromanski <bpiroman@hey.com>
2025-12-14 08:59:06 +0800

    first run

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("testing")
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("error getting current working director:", err)
+		return
+	}
+	fmt.Println("current working directory: ", cwd)
 }
